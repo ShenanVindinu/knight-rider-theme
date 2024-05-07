1c5523e8017edc93d4fff6b9c59f53c7972acc61
ShenanVindinu <shenanvindinu@gmail.com>
2024-05-07 19:43:53 +0530

    Jqueries refactored

---
+++ asset/js/start&stop.js
@@ -0,0 +1,37 @@
+$("#start").eq(0).on('click', () => {
+    $("section>div").css({display: 'block'});
+    const duration = 2;
+    const delayBetweenAnimations = 0.1;
+    const totalElements = 7;
+    const delayBetweenDirections = 3000; // 3 seconds delay between directions
+
+    const playLeftToRight = () => {
+        for (let i = 1; i <= totalElements; i++) {
+            const delay = i * delayBetweenAnimations;
+            $("#" + i).css("animation", `lightShow ${duration}s ${delay}s ease-in-out infinite`);
+        }
+    };
+
+
+    const playRightToLeft = () => {
+        for (let i = totalElements; i >= 1; i--) {
+            const delay = (totalElements - i + 1) * delayBetweenAnimations; // Reversing the delay
+            $("#" + i).css("animation", `lightShow ${duration}s ${delay}s ease-in-out infinite`);
+        }
+    };
+
+
+    const loopAnimations = () => {
+        playLeftToRight();
+        setTimeout(() => {
+            playRightToLeft();
+            setTimeout(loopAnimations, delayBetweenDirections);
+        }, 1000);
+    };
+
+    loopAnimations();
+});
+
+$("#stop").eq(0).on('click', () => {
+    $("section>div").css({display: 'none'});
+});
